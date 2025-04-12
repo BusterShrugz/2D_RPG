fa8bc5f769d6f083616a358d966328a838fe9d29
Reese's Pieces <reeseedens@gmail.com>
2025-04-12 16:31:01 -0600

    adding assets so I can work on laptop

---
+++ customTiles_16x16/SCIFI.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="SCIFI" tilewidth="16" tileheight="16" spacing="5" margin="5" tilecount="17745" columns="169">
+ <image source="../../../../DLs/Tech Dungeon Roguelite - Asset Pack (v7)/tileset x3.png" width="3552" height="2208"/>
+</tileset>
