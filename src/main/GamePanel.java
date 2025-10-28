53d838ab21319b2d1f98d6793ef54c7ecf75eb83
BusterShrugz <reeseedens@gmail.com>
2025-10-28 08:02:48 -0600

    Added start and pause screen. With BG image -RE

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,16 +1,15 @@
 package main;
 
 import entity.Player;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 import tileStart.TileManager;
 
-import javax.swing.JPanel;
-import java.awt.*;
 
-
-//---------------------------------------------------------------------------------------
 public class GamePanel extends JPanel implements Runnable {
-//---------------------------------------------------------------------------------------
-
     // SCREEN SETTINGS
     final int originalTileSize = 16;  // 16x16 tile grid
     final int scale = 3; // this 16 x 3 (scale) will make the sprite 48x48 pixels
@@ -21,36 +20,51 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol; //  768 pixels ->  48pixels x 16pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 Pixels ->  36pixels x 16pixels
 
+    BufferedImage titleImage;
+
     //FPS
     int FPS = 60;
 
+    
+
     TileManager tileM = new TileManager(this);
-    KeyHandler keyH = new KeyHandler();
+    KeyHandler keyH = new KeyHandler(this);
     Thread gameThread;
     Player player = new Player(this,keyH);
 
 
+    public int gameState;
+    public final int TITLE_SCREEN = 0;
+    public final int PLAY_SCREEN = 1;
+    public final int PAUSE_SCREEN = 2;
+    
 
-    //---------------------------------------------------------------------------------------
-    public GamePanel() {
-    //---------------------------------------------------------------------------------------
+    public GamePanel() 
+    {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.lightGray);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+
+        gameState = TITLE_SCREEN;
+
+         try {
+        titleImage = ImageIO.read(getClass().getResourceAsStream("/screenAssets/titlescreen.jpg"));
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
     }
 
-    //---------------------------------------------------------------------------------------
-    public void startGameThread(){
-    //---------------------------------------------------------------------------------------
+    public void startGameThread()
+    {
         gameThread = new Thread(this);
         gameThread.start();
     }
-    //---------------------------------------------------------------------------------------
+
     @Override
-    public void run(){
-    //---------------------------------------------------------------------------------------
+    public void run()
+    {
         double drawInterval = 1000000000/ FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
@@ -58,13 +72,15 @@ public class GamePanel extends JPanel implements Runnable {
         long timer = 0;
         int drawCount = 0;
 
-        while(gameThread != null) {
+        while(gameThread != null) 
+        {
             currentTime = System.nanoTime();
 
             delta += (currentTime - lastTime) / drawInterval;
             timer += (currentTime - lastTime);
             lastTime = currentTime;
-            if(delta >= 1) {
+            if(delta >= 1) 
+            {
                 // 1 UPDATE: update information; char position
                 update();
                 // 2 DRAW: draw screen with the updated information
@@ -72,52 +88,88 @@ public class GamePanel extends JPanel implements Runnable {
                 delta--;
                 drawCount++;
             }
-            if (timer >= 1000000000) {
+            if (timer >= 1000000000) 
+            {
                 System.out.println("FPS: " + drawCount);
                 drawCount = 0;
                 timer = 0;
             }
-          }
+        }
     }
-    //---------------------------------------------------------------------------------------
-    public void update() {
-    //---------------------------------------------------------------------------------------
-        player.update();
+
+    public void update() 
+    {
+        if(gameState == PLAY_SCREEN)
+        {
+            player.update();
+        }
     }
-    //---------------------------------------------------------------------------------------
-    public void paintComponent(Graphics g){
-    //---------------------------------------------------------------------------------------
+
+    public void paintComponent(Graphics g)
+    {
         super.paintComponent(g);
-
-        Graphics2D g2 = (Graphics2D)g;
-        tileM.draw(g2);
-        player.draw(g2);
+        Graphics2D g2 = (Graphics2D) g;
+        if(gameState == TITLE_SCREEN)
+        {
+            drawTitleScreen(g2);
+        } else if (gameState == PLAY_SCREEN)
+        {
+            tileM.draw(g2);
+            player.draw(g2);
+        }else if (gameState == PAUSE_SCREEN) {
+            drawPauseScreen(g2);
+        }
         g2.dispose();
+    }
 
+    public void drawTitleScreen(Graphics2D g2)
+    {
+
+           // Draw background image
+        if (titleImage != null) 
+        {
+            g2.drawImage(titleImage, 0, 0, screenWidth, screenHeight, null);
+        } else 
+        {
+            // fallback color
+            g2.setColor(Color.BLACK);
+            g2.fillRect(0, 0, screenWidth, screenHeight);
+        }
+            // Draw title text
+        g2.setColor(Color.ORANGE);
+        g2.setFont(new Font("Arial", Font.BOLD, 96));
+        String gameTitle = "TIME LEAP";
+        int x = getCenteredX(g2, gameTitle);
+        int y = screenHeight / 2 - 50;
+        g2.drawString(gameTitle, x, y);
+
+        g2.setFont(new Font("Arial", Font.BOLD, 36));
+        String startButton = "PRESS ENTER TO START";
+        x = getCenteredX(g2, startButton);
+        y += 100;
+        g2.drawString(startButton, x, y);
+    }
+
+    private int getCenteredX(Graphics2D g2, String text)
+    {
+        int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        return screenWidth / 2 - length / 2;
+    }
+
+    public void drawPauseScreen(Graphics2D g2) 
+    {
+        g2.setColor(Color.black);
+        g2.setFont(new Font("Arial", Font.BOLD, 96));
+        String text = "GAME PAUSED";
+        int x = getCenteredX(g2, text);
+        int y = screenHeight / 2;
+        g2.drawString(text, x, y);
+
+        g2.setFont(new Font("Arial", Font.BOLD, 36));
+        String resume = "Press ESC to Resume";
+        x = getCenteredX(g2, resume);
+        y += 50;
+        g2.drawString(resume, x, y);
     }
 }
 
-
-//    public void run() {
-//
-//        double drawInterval = 1000000000/FPS; //0.01666 seconds
-//        double nextDrawTime = System.nanoTime() + drawInterval;
-//
-//
-
-//
-//            try {
-//                double remainingTime = nextDrawTime - System.nanoTime();
-//                remainingTime = remainingTime/1000000;
-//
-//                if(remainingTime < 0){
-//                    remainingTime = 0;
-//                }
-//                Thread.sleep((long) remainingTime);
-//
-//                nextDrawTime += drawInterval;
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -3,35 +3,59 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 
-//---------------------------------------------------------------------------------------
 public class KeyHandler implements KeyListener {
-//---------------------------------------------------------------------------------------
     public boolean upPressed, downPressed, leftPressed, rightPressed;
+    GamePanel gp;
 
+    public KeyHandler(GamePanel gp) {
+        this.gp = gp;
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
     }
 
-//---------------------------------------------------------------------------------------
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
+        
+            // --- Title Screen Controls ---
+          if(code == KeyEvent.VK_ENTER) 
+          {
+            if(gp.gameState == gp.TITLE_SCREEN) 
+            {
+                gp.gameState = gp.PLAY_SCREEN;
+            }
+        }
+            // --- Gameplay Controls ---
+        else if (gp.gameState == gp.PLAY_SCREEN) 
+        {   
+            if (code == KeyEvent.VK_W) {
+                upPressed = true;
+            }
+            if (code == KeyEvent.VK_A) {
+                leftPressed = true;
+            }
+            if (code == KeyEvent.VK_S) {
+                downPressed = true;
+            }
+            if (code == KeyEvent.VK_D) {
+                rightPressed = true;
+            }
+            if (code == KeyEvent.VK_ESCAPE) {
+                gp.gameState = gp.PAUSE_SCREEN;
+            }
+        }
+             // --- Pause Controls ---
+        else if (gp.gameState == gp.PAUSE_SCREEN) {
+            if (code == KeyEvent.VK_ESCAPE) {
+                gp.gameState = gp.PLAY_SCREEN; // resume
+            }
+        }
 
-        if (code == KeyEvent.VK_W) {
-            upPressed = true;
-        }
-        if (code == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
-        if (code == KeyEvent.VK_S) {
-            downPressed = true;
-        }
-        if (code == KeyEvent.VK_D) {
-            rightPressed = true;
-        }
     }
-//---------------------------------------------------------------------------------------
+
+
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
