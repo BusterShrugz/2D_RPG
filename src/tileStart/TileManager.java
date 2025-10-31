c199654b4d3d1f3a57edee624560a4bae8b1a114
BusterShrugz <reeseedens@gmail.com>
2025-10-30 20:53:24 -0600

    Fixed git ignore so people can run local on their machine.

--- src/tileStart/TileManager.java
+++ src/tileStart/TileManager.java
@@ -18,7 +18,7 @@ public class TileManager {
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
-        loadMap("maps/FLOOR_ONE.txt");
+        loadMap("maps/spawnZoneCSVs/FLOOR_ONE.txt");
 
     }
 
@@ -53,9 +53,9 @@ public class TileManager {
     public void loadMap(String mapFileName) {
         try {
             // Load map file from classpath instead of using absolute path
-            InputStream is = getClass().getClassLoader().getResourceAsStream("maps/FLOOR_ONE.txt");
+            InputStream is = getClass().getClassLoader().getResourceAsStream("maps/spawnZoneCSVs/FLOOR_ONE.txt");
             if (is == null) {
-                System.out.println("Map file not found: " + "maps/FLOOR_ONE.txt");
+                System.out.println("Map file not found: " + "maps/spawnZoneCSVs/FLOOR_ONE.txt");
                 return;
             }
 
